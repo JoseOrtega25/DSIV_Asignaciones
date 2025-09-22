31eb1b5351ba605f92fbad045270a98f62e5dd71
JoséOrtega <jose.ortega23@utp.ac.pa>
2025-09-22 13:17:22 -0500

    "SuperHeroeApp"

---
+++ ConsoleAppPOO/ConsoleAppPOO/Models/SuperPoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPOO.Models
+{
+    internal class SuperPoder
+    {
+
+            public string nombre;
+            public string Descripcion;
+            public string NivelPoder Nivel;
+
+
+    public SuperPoder()
+            {
+                NivelPoder = NivelPoder.NivelUno;
+            }
+        }
+
+
+    }
+
---
+++ ConsoleAppPOO/ConsoleAppPOO/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using ConsoleAppPOO.Models;
+
+var poderVolar = new SuperPoder();
+poderVolar.Nombre ="Volar";
+poderVolar.Descripcion =  "Capacidad de volar y planear en el aire";
+poderVolar.Nivel = NivelPoder.NivelDos;
+
+var superFuerza = new SuperPoder();
+superFuerza.Nombre = "Super Fuerza" +
+poderVolar.Nivel = NivePoder.NivelTres;
+
+var superman = new SuperHeroe();
+superman.Id = 1;
+superman.Nombre = "Superman";
+superman.IdentidadSecreta = "Clark Kent";
+superman.Ciudad = "Metropolis";
+superman.PuedeVolar = true;
+
+var superman2 = new SuperHeroe();
+superman2.Id = 1;
+superman2.Nombre = "Superman";
+superman2.IdentidadSecreta = "Clark Kent";
+superman2.Ciudad = "Metropolis";
+superman2.PuedeVolar = true;
+
+List<SuperPoder> poderesSuperman = new List<SuperPoder>();
+poderesSuperman.Add(poderVolar);
+poderesSuperman.Add(superFuerza);
+superman.SuperPoderes = poderesSuperman;
+string resultSuperPoderes = superman.UsarSuperPoderes();
+Console.WriteLine(resultSuperPoderes);
+
+enum NivelPoder
+{
+    NivelUno,
+    NivelDos,
+    NivelTres
+}
+
+
+
+
